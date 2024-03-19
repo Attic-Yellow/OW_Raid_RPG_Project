1be865ffa565bfa3c19cf5d6139a7072c1438cf9
JSChoi99 <139032888+JSChoi99@users.noreply.github.com>
2024-03-19 13:20:27 +0900

    스크립트 수정 및 패키지 추가
    테스트 씬 생성,
    데어티 매니저.CS 수정
    포톤매니저,CS 수정,
    유저.CS 수정,
    파이어베이스 JSON파일 교체

--- Assets/Scripts/Alive/DefalutState.cs
+++ Assets/Scripts/Alive/DefalutState.cs
@@ -19,4 +19,107 @@ public class DefalutState : MonoBehaviour
     [SerializeField] protected float hpRegen; //체력 회복력
     [SerializeField] protected float damageReduc; //데미지 감소율
     [SerializeField] protected float maxHP;//최대 체력
+    public float Power
+    {
+        get { return power; }
+        set { power = value; }
+    }
+
+    // luck 프로퍼티
+    public float Luck
+    {
+        get { return luck; }
+        set { luck = value; }
+    }
+
+    // agility 프로퍼티
+    public float Agility
+    {
+        get { return agility; }
+        set { agility = value; }
+    }
+
+    // intellect 프로퍼티
+    public float Intellect
+    {
+        get { return intellet; }
+        set { intellet = value; }
+    }
+
+    // mentality 프로퍼티
+    public float Mentality
+    {
+        get { return mentality; }
+        set { mentality = value; }
+    }
+
+    // criticalRate 프로퍼티
+    public float CriticalRate
+    {
+        get { return criticalRate; }
+        set { criticalRate = value; }
+    }
+
+    // hitRate 프로퍼티
+    public float HitRate
+    {
+        get { return hitRate; }
+        set { hitRate = value; }
+    }
+
+    // pDef 프로퍼티
+    public float PDef
+    {
+        get { return pDef; }
+        set { pDef = value; }
+    }
+
+    // mDef 프로퍼티
+    public float MDef
+    {
+        get { return mDef; }
+        set { mDef = value; }
+    }
+
+    // castingSpeed 프로퍼티
+    public float CastingSpeed
+    {
+        get { return castingSpeed; }
+        set { castingSpeed = value; }
+    }
+
+    // magicCastingSpeed 프로퍼티
+    public float MagicCastingSpeed
+    {
+        get { return magicCastingSpeed; }
+        set { magicCastingSpeed = value; }
+    }
+
+    // manaRegen 프로퍼티
+    public float ManaRegen
+    {
+        get { return manaRegen; }
+        set { manaRegen = value; }
+    }
+
+    // hpRegen 프로퍼티
+    public float HPRegen
+    {
+        get { return hpRegen; }
+        set { hpRegen = value; }
+    }
+
+    // damageReduc 프로퍼티
+    public float DamageReduc
+    {
+        get { return damageReduc; }
+        set { damageReduc = value; }
+    }
+
+    // maxHP 프로퍼티
+    public float MaxHP
+    {
+        get { return maxHP; }
+        set { maxHP = value; }
+    }
 }
