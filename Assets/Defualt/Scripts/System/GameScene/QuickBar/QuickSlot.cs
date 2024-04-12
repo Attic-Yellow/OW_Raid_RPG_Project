a67587fb7351440c1e078d86abf6e0009273cf83
loftdoggy <chtori1202@gmail.com>
2024-04-12 12:26:20 +0900

    기능 추가 :
    - 퀵 슬롯 기능 추가 (진행중)

--- Assets/Defualt/Scripts/System/GameScene/QuickBar/QuickSlot.cs
+++ Assets/Defualt/Scripts/System/GameScene/QuickBar/QuickSlot.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEditor.Experimental.GraphView;
 
 public class QuickSlot : Slot
 {
@@ -12,15 +13,40 @@ public class QuickSlot : Slot
 
     public override void UpdateSlotUI()
     {
+        if (slot == null)
+        {
+            ClearSlot();
+            return;
+        }
+
         switch (slot.slotType)
         {
             case SlotType.Item:
-                break;
             case SlotType.Equipment:
-                itemIcon.sprite = slot.GetEquipment().itemImage;
-                itemIcon.gameObject.SetActive(true);
+                // 아이템 또는 장비 슬롯 참조
+                Equipment equipment = slot.GetEquipment();
+                if (equipment != null)
+                {
+                    itemIcon.sprite = equipment.itemImage;
+                    itemIcon.gameObject.SetActive(true);
+                }
+                else
+                {
+                    ClearSlot();
+                }
                 break;
             case SlotType.Skill:
+                // 스킬 슬롯 참조
+                //Skill skill = slot.GetSkill(); // GetSkill() 메소드가 있다고 가정
+                //if (skill != null)
+                //{
+                //    itemIcon.sprite = skill.skillIcon;
+                //    itemIcon.gameObject.SetActive(true);
+                //}
+                //else
+                //{
+                //    ClearSlot();
+                //}
                 break;
         }
     }
@@ -30,5 +56,19 @@ public class QuickSlot : Slot
         slot = null;
         itemIcon.sprite = null;
         itemIcon.gameObject.SetActive(false);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject droppedObject = eventData.pointerDrag;
+        if (droppedObject != null)
+        {
+            Slot slot = droppedObject.GetComponent<Slot>();
+            if (slot != null)
+            {
+                this.slot = slot;
+                UpdateSlotUI();
+            }
+        }
     }
 }
